e79c31ad5006f7d8f86303bb6c328e97eea2f8f5
RandomWebsite101 <littlebabydallas@hotmail.com>
2026-04-29 19:18:31 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-
+const CACHE='rbl-v2';
+const URLS=['/','/index.html','/manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res.status===200){const c=res.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c))}return res}).catch(()=>caches.match('/index.html'))))});
